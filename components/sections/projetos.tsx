b3521112c4d85fa54877534a08cb077e2cb182b2
glummer10 <ferreirajerson81@gmail.com>
2024-10-30 15:09:35 -0300

    update projetos

--- components/sections/projetos.tsx
+++ components/sections/projetos.tsx
@@ -19,9 +19,9 @@ export default function Projetos() {
 
                 {projetos.map((project) => (
                     <Link href={`${project.link}`}>
-                        <div key={project.name} className="border border-stone-900 rounded-lg p-6 animate-pulse">
-                            <div className="h-6 bg-gray-300 rounded mb-4"></div>
-                            <div className="h-4 bg-gray-300 rounded mb-2"></div>
+                        <div key={project.name} className="border border-stone-900 rounded-lg p-6">
+                            <div className="h-6 bg-gray-300 rounded mb-4">{project.name}</div>
+                            <div className="h-4 bg-gray-300 rounded mb-2">{project.description}</div>
                             <div className="h-4 bg-gray-300 rounded mb-4"></div>
                             <div className="h-4 bg-gray-300 rounded w-1/2"></div>
                         </div>
