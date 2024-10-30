076c1a66cfadba2d5addc77602c35dc0d91ff381
glummer10 <ferreirajerson81@gmail.com>
2024-10-30 15:55:26 -0300

    updated a footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,9 @@
 export default function Footer() {
+    const year = new Date().getFullYear();
     return (
         <footer className="py-6">
             <div className="container mx-auto px-6 text-center text-gray-600 dark:text-gray-400">
-                <p>&copy; 2023 Gedson.</p>
+                <p>&copy; {year} Gedson.</p>
             </div>
         </footer>
     )
