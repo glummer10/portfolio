f2d95fcce9b18c5bf547e6c69bbf9cf934869b7f
gedsonb <ferreirajerson81@gmail.com>
2024-10-30 14:16:13 -0300

    tested]

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
         </section>
 
         <ContactSection />
-
+        
         <footer className="bg-black py-6">
           <div className="container mx-auto px-6 text-center text-gray-600 dark:text-gray-400">
             <p>&copy; 2023 Gedson.</p>
